5ab2910159f7926e379058a3d71bd2af6942d6cd
jayeshkumarpatel2611 <104490446+jayeshkumarpatel2611@users.noreply.github.com>
2022-04-29 11:42:38 +0530

    Delete test.tf

--- test.tf
+++
@@ -1,9 +0,0 @@
-module "app_service_test" {
-
-    source = "./modules/"
-    app_service_plan_name = "plan-dev"
-    app_service_name = "mydemowebapp02"
-    resource_group_name = "app-service-dev-rg"
-    resource_group_location = "eastus"
-  
-}

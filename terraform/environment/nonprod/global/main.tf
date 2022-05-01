6a64a7873ad2d5c830e48fab04979bc9596675ce
jayeshkumarpatel2611 <104490446+jayeshkumarpatel2611@users.noreply.github.com>
2022-05-01 14:55:48 +0530

    Update main.tf

--- terraform/environment/nonprod/global/main.tf
+++ terraform/environment/nonprod/global/main.tf
@@ -30,10 +30,14 @@ app_insight_name = var.app_insight_name
 analytics_name = var.analytics_name
 analytics_sku = var.analytics_sku
 
-Global_Key_Vault_Name = var.Global_Key_Vault_Name
-Global_Key_Vault_RG = var.Global_Key_Vault_RG
-Key_Vault_Name = var.Key_Vault_Name
+# Global_Key_Vault_Name = var.Global_Key_Vault_Name
+# Global_Key_Vault_RG = var.Global_Key_Vault_RG
+# Key_Vault_Name = var.Key_Vault_Name
 
+KEY_VAULT_NAME            = var.KEY_VAULT_NAME
+KEY_VAULT_RGNAME          = var.KEY_VAULT_RGNAME
+keyvault_name             = var.keyvault_name
+  
 server_name = var.server_name
 database_name = var.database_name
 
